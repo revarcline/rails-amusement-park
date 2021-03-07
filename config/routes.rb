ad5e95ff1477cdc307150cd856f369d7f73e11b7
Alex Cline <rev.a.r.cline@gmail.com>
2021-03-07 11:04:32 -0600

    commit tree dc21331ecf5b933dbf31fa041571ada5d50b48dd

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'static#home'
+  get '/signin', to: 'session#new', as: 'signin'
+  delete '/session', to: 'session#destroy'
+  post '/rides', to: 'rides#create', as: 'rides'
+  resources :users
+  resources :attractions
+  resources :session, only: %i[create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,9 +18,10 @@ RSpec.configure do |config|
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
 
+  config.tty = true
+  config.color = true
 
   config.expect_with :rspec do |expectations|
-
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
@@ -39,7 +40,4 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
-
-  
-
 end
